8daf4e34ee7e36f6a09cd9b69e0cd79ca101bb71
Gregory Arcentales <gregoarcenta@gmail.com>
2025-01-16 12:46:27 -0500

    jest config added

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,21 +1,21 @@
-import {ComponentFixture, TestBed} from '@angular/core/testing';
-import {AppComponent} from '@/app.component';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
   let fixture: ComponentFixture<AppComponent>;
   let component: AppComponent;
   let compiled: HTMLElement;
-  
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [AppComponent],
     }).compileComponents();
-    
+
     fixture = TestBed.createComponent(AppComponent);
     component = fixture.componentInstance;
     compiled = fixture.nativeElement as HTMLElement;
   });
-  
+
   it('should create the AppComponent', () => {
     expect(component).toBeTruthy();
   });
