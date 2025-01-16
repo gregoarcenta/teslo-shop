237bed3484519dcf73a8c85fa135d6e6a3ff5590
Gregory Arcentales <gregoarcenta@gmail.com>
2025-01-16 10:33:35 -0500

    initial config

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,8 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  imports: [],
+  template: ` <h1>Hola Mundo</h1> `,
 })
-export class AppComponent {
-  title = 'teslo-shop';
-}
+export class AppComponent {}
