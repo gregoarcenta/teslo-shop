c6d111698aedf9cb932e353b7245c1a516099f38
Gregory Arcentales <gregoarcenta@gmail.com>
2025-01-19 16:25:42 -0500

    refactor: message session expired added

--- src/app/core/services/auth.service.ts
+++ src/app/core/services/auth.service.ts
@@ -1,10 +1,17 @@
-import { inject, Injectable, InjectionToken, signal } from '@angular/core';
+import {
+  inject,
+  Injectable,
+  InjectionToken,
+  PLATFORM_ID,
+  signal,
+} from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from '../../../environments/environment.development';
 import { catchError, map, Observable, of, shareReplay } from 'rxjs';
 import { IApiResponse, IAuthUser, IUser } from '@/core/models';
-import { Router } from '@angular/router';
 import { SsrCookieService } from 'ngx-cookie-service-ssr';
+import { ToastService } from '@/core/services/toast.service';
+import { isPlatformBrowser } from '@angular/common';
 
 export const SERVER_JWT_TOKEN = new InjectionToken<string>('SERVER_JWT_TOKEN');
 export const JWT_TOKEN_NAME = 'teslo_auth_token';
@@ -24,8 +31,9 @@ export class AuthService {
   // Services
   private readonly serverJwt = inject(SERVER_JWT_TOKEN, { optional: true });
   private readonly cookie = inject(SsrCookieService);
+  private readonly toastService = inject(ToastService);
+  private readonly platformId = inject(PLATFORM_ID);
   private readonly http = inject(HttpClient);
-  private readonly router = inject(Router);
 
   isAuthenticated(): Observable<boolean> {
     const token =
@@ -50,8 +58,15 @@ export class AuthService {
           this.user.set(user);
           return true;
         }),
-        catchError(() => {
+        catchError(({ error }) => {
+          console.error(error.message);
           this.logout();
+          if (isPlatformBrowser(this.platformId)) {
+            this.toastService.showToast(
+              'Your session has expired. Please log in again.',
+              'warning',
+            );
+          }
           return of(false);
         }),
       );
@@ -72,6 +87,5 @@ export class AuthService {
   logout() {
     this.user.set(null);
     this.cookie.delete(JWT_TOKEN_NAME);
-    this.router.navigateByUrl('/login').then((_) => {});
   }
 }
